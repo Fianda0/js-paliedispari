a797434a49008bc934eef33d7c36f03847ec67df
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-04-29 15:02:13 +0200

    Palindroma

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+let parola = prompt('inserire parola')
+console.log(parola)
+console.log(parola.length)
+
+// Creo cicolo per confrontare le lettere della parola
+for (let i = 0; i < parola.length; i++) {
+
+}
