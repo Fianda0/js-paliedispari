c35d7834bd5f038b5b239f58c8d93ce8d2c71a44
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-04-29 15:17:38 +0200

    confronto lettere e stampa risultato

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 let parola = prompt('inserire parola')
 console.log(parola)
 console.log(parola.length)
+let risultato = 'palindroma'
 
 // Creo cicolo per confrontare le lettere della parola
 for (let i = 0; i < parola.length; i++) {
@@ -12,7 +13,15 @@ for (let i = 0; i < parola.length; i++) {
     //creo variabile Y per lettere decrescenti
     let y = parola[parola.length - 1 - i]
 
+    //se le lettere non coincidono stampare risultato NON PALINDROMA
+    if (x != y) {
+        risultato = 'non palindroma'
+        break
+    }
+
     console.log('x', x)
     console.log('y', y)
 
 }
+//Stampa Risultato
+console.log(risultato)
