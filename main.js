4d1e5bb7ecfd246b2f5c76c375ff249abe0c9636
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-04-29 15:11:00 +0200

    creazione variabili per le lettere

--- main.js
+++ main.js
@@ -7,4 +7,12 @@ console.log(parola.length)
 // Creo cicolo per confrontare le lettere della parola
 for (let i = 0; i < parola.length; i++) {
 
+    //creo variabile X per lettere crescenti
+    let x = parola[i]
+    //creo variabile Y per lettere decrescenti
+    let y = parola[parola.length - 1 - i]
+
+    console.log('x', x)
+    console.log('y', y)
+
 }
